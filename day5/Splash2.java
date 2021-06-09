3cb17d0bfa47dd6542cc7099f322351eee5d3bd6
Arjav <75114434+arjav208@users.noreply.github.com>
2021-06-09 18:05:23 +0530

    Add files via upload

---
+++ day5/Splash2.java
@@ -0,0 +1,52 @@
+package com.example.splashactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Splash2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+
+                try {
+                    setContentView(R.layout.activity_main);
+                    sleep(3000);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    Intent intent = new Intent(Splash2.this, Splash3.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        ourSound.release();
+        finish();
+
+    }
+    }
